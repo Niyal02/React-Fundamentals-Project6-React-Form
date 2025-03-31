a17371233f096ac14801379e3d06be1bdf4a598c
Niyal02 <bhetwalniyal@gmail.com>
2025-03-31 09:51:08 +0545

    Dashboard layout page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginForm from "./components/login_register_form/LoginForm";
 import RegisterForm from "./components/login_register_form/RegisterForm";
-import ErrorPage from "./components/404";
+// import ErrorPage from "./components/404";
 import Dashboard from "./components/dashboard/Dashboard";
 import { Toaster } from "react-hot-toast";
 // import PrivateRoute from "./routes/PrivateRoute";
 import PublicRoute from "./routes/PublicRoute";
-import Category from "./components/items/Category";
-import Products from "./components/items/Products";
+import Category from "./components/pages/Category";
+import Products from "./components/pages/Products";
+import DashboardLayout from "./components/dashboard/DashboardLayout";
+import Payment from "./components/pages/Payment";
+import OrderHistory from "./components/pages/OrderHistory";
 
 function App() {
   return (
@@ -17,12 +20,16 @@ function App() {
           <Route path="/" element={<LoginForm />} />
           <Route path="/register" element={<RegisterForm />} />
         </Route>
-        <Route element={<PublicRoute />}>
-          <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/user" element={<DashboardLayout />}>
+          <Route element={<PublicRoute />}>
+            <Route path="/user/dashboard" element={<Dashboard />} />
+          </Route>
+          <Route path="/user/category" element={<Category />} />
+          <Route path="/user/products" element={<Products />} />
+          <Route path="/user/payment" element={<Payment />} />
+          <Route path="/user/order-history" element={<OrderHistory />} />
+          {/* <Route path="/404-Page-not-found" element={<ErrorPage />} /> */}
         </Route>
-        <Route path="/category" element={<Category />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/404-Page-not-found" element={<ErrorPage />} />
       </Routes>
       <Toaster
         toastOptions={{
---
+++ src/components/dashboard/DashboardLayout.tsx
@@ -0,0 +1,125 @@
+import { JSX, useState } from "react";
+import Logout from "../logout/Logout";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { FiHome } from "react-icons/fi";
+import { MdOutlineCategory } from "react-icons/md";
+import { GoBriefcase } from "react-icons/go";
+import { GrMoney } from "react-icons/gr";
+import { GoHistory } from "react-icons/go";
+import { Link, Outlet } from "react-router-dom";
+
+const DashboardLayout = () => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [activeItem, setActiveItem] = useState("Dashboard");
+
+  return (
+    <div className=" flex">
+      <div
+        className={`bg-gray-700 h-screen p-4 flex flex-col text-white transition-all duration-400 ${
+          isCollapsed ? "w-16" : "w-56"
+        }`}
+      >
+        <button
+          className="mb-4 ml-1.5 text-xl"
+          onClick={() => setIsCollapsed(!isCollapsed)}
+        >
+          {isCollapsed ? <FaBars /> : <FaTimes />}
+        </button>
+
+        <nav className="flex flex-col gap-4">
+          <div>
+            <NavItem
+              to="/user/dashboard"
+              icon={<FiHome />}
+              text="Dashboard"
+              isCollapsed={isCollapsed}
+              activeItem={activeItem}
+              setActiveItem={setActiveItem}
+            />
+            <NavItem
+              to="/user/category"
+              icon={<MdOutlineCategory />}
+              text="Category"
+              isCollapsed={isCollapsed}
+              activeItem={activeItem}
+              setActiveItem={setActiveItem}
+            />
+            <NavItem
+              to="/user/products"
+              icon={<GoBriefcase />}
+              text="Products"
+              isCollapsed={isCollapsed}
+              activeItem={activeItem}
+              setActiveItem={setActiveItem}
+            />
+            <NavItem
+              to="/user/payment"
+              icon={<GrMoney />}
+              text="Payments"
+              isCollapsed={isCollapsed}
+              activeItem={activeItem}
+              setActiveItem={setActiveItem}
+            />
+            <NavItem
+              to="/user/order-history"
+              icon={<GoHistory />}
+              text="Order History"
+              isCollapsed={isCollapsed}
+              activeItem={activeItem}
+              setActiveItem={setActiveItem}
+            />
+          </div>
+          <div
+            className={`relative flex  mt-90 p-2 rounded cursor-pointer transition-all duration-350 size-2${
+              isCollapsed ? "w-16 justify-center" : "w-full"
+            } hover:w-30 hover:bg-gray-600 `}
+          >
+            <Logout />
+          </div>
+        </nav>
+      </div>
+
+      <div className="flex-1 p-6 bg-[#dad5cb] min-h-screen">
+        <Outlet />
+      </div>
+    </div>
+  );
+};
+
+const NavItem = ({
+  to,
+  icon,
+  text,
+  isCollapsed,
+  activeItem,
+  setActiveItem,
+}: {
+  to: string;
+  icon: JSX.Element;
+  text: string;
+  isCollapsed: boolean;
+  activeItem: string;
+  setActiveItem: (item: string) => void;
+}) => (
+  <Link to={to}>
+    <div
+      className={`relative flex items-center mt-4 p-2 rounded cursor-pointer transition-all duration-350 ${
+        isCollapsed ? "w-16 justify-center" : "w-full"
+      } hover:w-50 hover:bg-gray-600 ${
+        activeItem === text ? "border border-gray-600 bg-gray-600" : ""
+      }`}
+      onClick={() => setActiveItem(text)}
+    >
+      {icon}
+      <span
+        className={` absolute left-12 ${
+          isCollapsed ? "opacity-0 group-hover:opacity-100" : "opacity-100"
+        }`}
+      >
+        {text}
+      </span>
+    </div>
+  </Link>
+);
+
+export default DashboardLayout;
